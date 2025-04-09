d8128139bb92f58f6ad289968aa4fa789f8a8560
edi <polodeanu.eduard@gmail.com>
2025-04-09 13:55:53 +0300

    Add player role choice

--- lib/game.rb
+++ lib/game.rb
@@ -7,13 +7,31 @@ class Game
   ATTEMPTS = 8
   SLOTS = 4
   def initialize
-    @players = [Player.new("Player1", Codebreaker.new("human")), Player.new("Player2", Codemaker.new("computer"))]
+    @players = [Player.new("Player1", Role.new("human")), Player.new("Player2", Role.new("computer"))]
     @board = Board.new(ATTEMPTS)
     start_game
   end
 
   def start_game
+    choose_role
     start_round(@players)
+  end
+
+  def choose_role
+    puts "Do you want to be the guesser or the code creator? [codebreaker/codemaker]"
+    loop do
+      choice = gets.chomp.downcase
+      if choice == "codebreaker"
+        @players[0].role = Codebreaker.new("human")
+        @players[1].role = Codemaker.new("computer")
+        break
+      elsif choice == "codemaker"
+        @players[1].role = Codemaker.new("human")
+        @players[0].role = Codebreaker.new("computer")
+        break
+      end
+      puts "Your input is invalid. Choose from [codebreaker/codemaker]."
+    end
   end
 
   def start_round(players)
